d545da0e7cd8a33b0f5ca53cf185f98dad821ac0
MaitreyaManohar <103531625+MaitreyaManohar@users.noreply.github.com>
2023-01-31 12:00:47 +0530

    character list without pageination

--- anilist_flutter/lib/screens/character_list/character_list.dart
+++ anilist_flutter/lib/screens/character_list/character_list.dart
@@ -6,11 +6,24 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 class CharacterList extends StatelessWidget {
   String animeTitle = " ";
   String query = "";
+  final _scrollController = ScrollController();
+
+  int page = 1;
+  late ValueNotifier<GraphQLClient> client;
   CharacterList({super.key, required this.animeTitle}) {
+    final HttpLink httpLink = HttpLink("https://graphql.anilist.co/");
+    client = ValueNotifier<GraphQLClient>(
+      GraphQLClient(
+        link: httpLink,
+        cache: GraphQLCache(
+          store: HiveStore(),
+        ),
+      ),
+    );
     query = """
 query{
   Media(search:"${animeTitle.trim()}"){
-    characters {
+    characters(page:$page,perPage:10) {
       nodes{
         
         name {
@@ -33,10 +46,49 @@ query{
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("Characters of ${animeTitle.trim()}"),
+    return GraphQLProvider(
+      client: client,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Characters of ${animeTitle.trim()}"),
+        ),
+        body: Query(
+          options: QueryOptions(document: gql(query)),
+          builder: (result, {fetchMore, refetch}) {
+            
+            return NotificationListener(
+              onNotification: (notification) {
+                if(notification is ScrollEndNotification && _scrollController.position.pixels==_scrollController.position.maxScrollExtent){
+                  
+                }
+                return true;
+              },
+              child: ListView.builder(
+                  controller: _scrollController,
+                  itemCount: result.data?['Media']['characters']['nodes'].length,
+                  itemBuilder: ((context, index) {
+                    if (result.isLoading) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    } else {
+                      return ListTile(
+                        contentPadding: const EdgeInsets.all(20),
+                        leading: Image.network(
+                          result.data?['Media']['characters']['nodes'][index]
+                              ['image']['medium'],
+                          height: 100,
+                          fit: BoxFit.cover,
+                        ),
+                        title: Text(result.data?['Media']['characters']['nodes']
+                            [index]['name']['full']),
+                      );
+                    }
+                  })),
+            );
+          },
+        ),
       ),
     );
   }
